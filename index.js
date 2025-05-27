b187c0d8e3003cf9e4be3cd208e0f74796d877c6
hachemiu <hachemid73@gmail.com>
2025-05-27 23:21:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// JavaScript code can be added here if needed
+        console.log("Welcome to My Documentation");
+        document.addEventListener("DOMContentLoaded", function() {
+            console.log("Document is ready!");
+        });
+        // You can add more JavaScript functionality here
+// This is a simple JavaScript file that can be used to enhance the documentation
+// or add interactivity to the documentation page.  
